2e2ab097963fce5be75fb0a5fc09f12b7a47bbcb
Rishabh <rishabhsrivastavaluv@gmail.com>
2025-12-13 00:04:03 +0530

    My name

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { ModeToggle } from "@/components/mode-toggle";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Utsav Yadav",
+  title: "Rishabh Srivastava",
   description: "A portfolio site built with Next.js and Tailwind CSS.",
 };
 
